3682c98c12989a7e9948bbc224e464f56c632f9b
Rosie <rclongo@ucsc.edu>
2023-05-10 22:28:32 -0700

    starting scene switch shit

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,13 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    [SerializeField] public int sceneIndex;
     public Animator transition;
     public float transitionTime = 1f;
 
-    public void LoadNextLevel()
+    public void LoadNextLevel()//honestly this may only be useful when we start the game since we want to be able to access all levels whenever
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
+
+    public void LoadLevelArea() //like buildings
+    {
+        StartCoroutine(LoadLevel(sceneIndex));
+    }
+
+    //we can consider other areas of a domain that change the screen to be "levels" but buildings would be temporary areas <-map out
 
     IEnumerator LoadLevel(int levelIndex)
     {
